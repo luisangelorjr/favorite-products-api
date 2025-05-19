30cf3930be65154ddcce87c7843378500bb7ce9c
luisangelorjr <luisangelorjr@gmail.com>
2025-05-19 17:32:13 -0300

    BUILD: 'First commit'

---
+++ src/main/java/br/com/luisangelorjr/favorite_products_api/FavoriteProductsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.luisangelorjr.favorite_products_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriteProductsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoriteProductsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luisangelorjr/favorite_products_api/FavoriteProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luisangelorjr.favorite_products_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoriteProductsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
